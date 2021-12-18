86834379e017211e3168f12e4d22ca59912c9a39
desktop <kazuking.1911@gmail.com>
2021-12-18 20:04:43 +0900

    重複するデータを削除して結合する部分を関数切り出し

--- remove_duplicate_data.py
+++ remove_duplicate_data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-
+# todo:データフレーム二つ入れたら、いい感じに結合して返してくれるようにする
 def main():
     row_df = pd.read_csv('data/6554_20211215_1130.csv',
                          header=0, index_col=0, encoding='cp932')
@@ -51,6 +51,43 @@ def main():
     unique_df.to_csv(fname, encoding='cp932')
     print('正常終了')
 
+def remove_duplicate(original_df,input_df):
+    # １秒ごとに入ってくるデータ
+    input_df = input_df[input_df['時刻'] != '--------']
+    # 何もデータが入っていない時は無視
+    if input_df[0:1]['時刻'].values != '--------':
+        # からであればとりあえずコピー
+        if original_df.empty:
+            original_df = input_df.dropna()
+        else:
+            i = 0
+            flag = True
+            while flag:
+                # input_dfの中で一致する行を探す
+                while not compare_df(original_df[:1], input_df[i:i+1]) and i < len(input_df):
+                    i += 1
+
+                # 一致するのがなかったらすべて新しいデータなので全部追加
+                if i == len(input_df)+1:
+                    original_df = pd.concat(
+                        [input_df.dropna(), original_df]).reset_index(drop=True)
+                    original_df = original_df[original_df['時刻']
+                                                       != '--------']
+                    flag = False
+                else:
+                    # それから先も比べてみる 最後まで比べるべき？10個くらいでいいかも
+                    j = 1
+                    while j+i < len(input_df) and j < len(original_df):
+                        if not compare_df(original_df[j:j+1], input_df[i+j:i+j+1]):
+                            i += 1
+                            break
+                        j += 1
+                    # 一致(while文が正常終了)すればinput_dfのi番目のデータまでを連結する
+                    else:
+                        original_df = pd.concat(
+                            [input_df[:i], original_df]).reset_index(drop=True)
+                        flag = False
+    return original_df
 
 def compare_df(df1, df2):
     for index in df1.columns:
