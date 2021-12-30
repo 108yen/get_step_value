9a0ea85ac6342e0961dfa205f64f13e78e1e498e
mac <kazuking.1911@gmail.com>
2021-12-30 21:39:42 +0900

    出来高が多かった時の縮小処理追記

--- view.py
+++ view.py
@@ -38,7 +38,7 @@ def update_canvas():
     # 出来高表示のための変数
     buy_col = '#cd5c5c'
     sell_col = '#4169e1'
-    vol_mag=1200 # 倍率　これで割られる
+    vol_mag = 1200  # 倍率　これで割られる
     buy_volume = 0  # 出来高
     sell_volume = 0
     pre_value = 0  # 前の価格
@@ -48,8 +48,12 @@ def update_canvas():
     while ini_time > split5m.time():
         split5m = split5m+timedelta(minutes=5)
 
-    canvas.create_line(15, 450, 15, 450, width=5,fill=buy_col, tag='buy_volume0')
-    canvas.create_line(15, 450, 15, 450, width=5, fill=sell_col, tag='sell_volume0')
+    canvas.create_line(15, 450, 15, 450, width=5,
+                       fill=buy_col, tag='buy_volume0')
+    canvas.create_line(15, 450, 15, 450, width=5,
+                       fill=sell_col, tag='sell_volume0')
+    canvas.create_line(0,450-150,800,450-150)
+
     canvas.create_line(15, defy, 15, defy, tag='line0')
     canvas.create_rectangle(10, defy, 20, defy, fill='red', tag='rect0')
     canvas.create_text(60, defy, text='', tag='value')
@@ -92,9 +96,9 @@ def update_canvas():
             else:
                 buy_volume = 0
                 sell_volume = int(data['出来高'])
-            sell_sy=450
+            sell_sy = 450
             sell_fy = sell_sy-sell_volume//vol_mag
-            buy_sy=sell_fy
+            buy_sy = sell_fy
             buy_fy = buy_sy-buy_volume//vol_mag
             canvas.create_line(linex, buy_sy, linex, buy_fy, width=5, fill=buy_col,
                                tag='buy_volume'+str(minutes_num))
@@ -127,6 +131,23 @@ def update_canvas():
                           linex, buy_sy, linex, buy_fy)
             canvas.coords('sell_volume'+str(minutes_num),
                           linex, sell_sy, linex, sell_fy)
+            # 出来高が長すぎる時の処理
+            if sell_sy-buy_fy > 150:
+                vol_mag=int(vol_mag*1.1)
+                for i in range(minutes_num):
+                    sell_sx, sell_sy, sell_fx, sell_fy = canvas.coords(
+                        'sell_volume'+str(i))
+                    buy_sx, buy_sy, buy_fx, buy_fy = canvas.coords(
+                        'buy_volume'+str(i))
+                    sell_ln = ((sell_sy-sell_fy)*10)//11
+                    buy_ln = ((buy_sy-buy_fy)*10)//11
+                    sell_fy = sell_sy-sell_ln
+                    buy_sy = sell_fy
+                    buy_fy = buy_sy-buy_ln
+                    canvas.coords('sell_volume'+str(i), sell_sx,
+                                  sell_sy, sell_fx, sell_fy)
+                    canvas.coords('buy_volume'+str(i), buy_sx,
+                                  buy_sy, buy_fx, buy_fy)
         # 価格の表示
         canvas.itemconfig('value', text=data['約定値'])
         canvas.coords('value', fx+40, fy)
